5595a932b049a6a16b542ebd8f21c199b652d7b4
Qabasya <borovd@yandex.ru>
2026-05-29 18:44:42 +0200

    Убрана форма регистрации преподавателей

--- inc/Core/Activate.php
+++ inc/Core/Activate.php
@@ -101,7 +101,7 @@ class Activate {
 		// createPageIfNeeded() — создаёт страницу, если её ещё нет
 		// Параметры: enum страницы, заголовок, шорткод для вставки
 		$generator->createPageIfNeeded( PageRoutes::SignIn, 'Авторизация', ShortCode::LoginForm->tag() );
-		$generator->createPageIfNeeded( PageRoutes::SignUp, 'Регистрация', ShortCode::RegisterForm->tag() );
+//		$generator->createPageIfNeeded( PageRoutes::SignUp, 'Регистрация', ShortCode::RegisterForm->tag() ); // Временно не работает, можно заменить на apply
 		$generator->createPageIfNeeded( PageRoutes::UserProfile, 'Личный кабинет', ShortCode::Profile->tag() );
 		$generator->createPageIfNeeded(
 			PageRoutes::ConsentPage,
