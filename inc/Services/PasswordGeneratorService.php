d8c3fa5fd810153a045c9a5ac42b3834fd768c10
Qabasya <borovd@yandex.ru>
2026-06-03 16:48:43 +0200

    Работа над person-detail

--- inc/Services/PasswordGeneratorService.php
+++ inc/Services/PasswordGeneratorService.php
@@ -6,6 +6,7 @@ namespace Inc\Services;
 
 use Inc\Enums\AuditAction;
 use Inc\Managers\UserManager;
+use Inc\Repositories\OptionsRepositories\UserRepository;
 
 class PasswordGeneratorService {
     # TODO: как станет больше мета - добавить enum
@@ -15,6 +16,7 @@ class PasswordGeneratorService {
 		private readonly UserManager      $user_manager,
 		private readonly AuditService     $audit_service,
 		private readonly PiiCryptoService $crypto,
+		private readonly UserRepository $user_repository,
 	) {}
 
 	/**
@@ -35,7 +37,13 @@ class PasswordGeneratorService {
 		wp_set_password( $password, $user_id );
 
 		$encoded = base64_encode( $this->crypto->encrypt( $password ) );
-		update_user_meta( $user_id, self::META_KEY, $encoded );
+
+		$this->user_repository->updateMeta(
+			$user->$user_id,
+			array(
+				self::META_KEY => $encoded,
+			)
+		);
 
 		$this->audit_service->record(
 			AuditAction::PasswordGenerated->value,
